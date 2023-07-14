1e65911c32dab9574bbe5778e674d40ef5de49dd
Peter Oti <71887594+PetxCode@users.noreply.github.com>
2023-07-14 12:57:55 +0100

    second commit

--- mainApp.ts
+++ mainApp.ts
@@ -1,12 +1,14 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import auth from "./router/authRouter";
+import task from "./router/taskRouter";
 
 export const mainApp = (app: Application) => {
   app
     .use(cors())
     .use(express.json())
     .use("/api/v1/auth", auth)
+    .use("/api/v1/task", task)
 
     .get("/", (req: Request, res: Response) => {
       try {
--- src/dist/mainApp.js
+++ src/dist/mainApp.js
@@ -7,11 +7,13 @@ exports.mainApp = void 0;
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const authRouter_1 = __importDefault(require("./router/authRouter"));
+const taskRouter_1 = __importDefault(require("./router/taskRouter"));
 const mainApp = (app) => {
     app
         .use((0, cors_1.default)())
         .use(express_1.default.json())
         .use("/api/v1/auth", authRouter_1.default)
+        .use("/api/v1/task", taskRouter_1.default)
         .get("/", (req, res) => {
         try {
             res.status(200).json({
